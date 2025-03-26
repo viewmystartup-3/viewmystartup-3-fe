ffb75395ff9a55690da53fb98c2600ecff55f81e
Wooju <dnwn810@gmail.com>
2025-03-26 17:02:33 +0900

    Feat:투자 상세페이지 상단 부분 추가

---
+++ src/pages/companyDetailPage/CompanyDetailPage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CompanyDetails from "../../components/companyDetails/CompanyDetails";
+
+const CompanyDetailPage = ()=> {
+  return(
+    <div>
+      <CompanyDetails/>
+      
+    </div>
+  )
+}
+
+export default CompanyDetailPage;
