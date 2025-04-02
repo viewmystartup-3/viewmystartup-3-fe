65bb62b4577e9feb888311a21b816fcdbcc1e2e9
root <root@DESKTOP-AFSEKMO.>
2025-04-02 15:57:50 +0900

    Feat: table 만드는 중(백엔드 만들려고 커밋)

--- src/components/myPageTable/MyResultPage.jsx
+++ src/components/myPageTable/MyResultPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ResultTable from "./ResultTable";
+import MyCompanySection from "../../pages/myPage/MyCompanySection";
 
 function MyResultPage() {
   return (
     <main>
+      <MyCompanySection />
       <ResultTable />
     </main>
   );
--- src/components/myPageTable/ResultTable.jsx
+++ src/components/myPageTable/ResultTable.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./ResultTable.module.scss";
-import SelectBox from "../../components/selectBox/SelectBox";
-import { basicSortOptions } from "../../components/selectBox/sortOptions";
+import SelectBox from "../selectBox/SelectBox";
+import { basicSortOptions } from "../selectBox/sortOptions";
+import StartupList from "../startupList/StartupList";
+import axios from "axios";
+import { dataUrl } from "../../env.js";
 
-function ResultTable() {
+// 위에서 받을 props (=적용할 데이터)
+function ResultTable({ myCompany, compareCompanies }) {
+  const [loadedData, setLoadedData] = useState([]);
+
+  useEffect(() => {
+    if (!myCompany) return;
+
+    // 데이터 불러옴
+    const fetchData = async (sortOrder = "totalInvestment_desc") => {
+      try {
+        const selectedCompanyIds = [myCompany, ...compareCompanies]
+          .slice(0, 6)
+          .map((company) => company.id);
+
+        const response = await axios.get(`${dataUrl}/api/companies`, {
+          params: { sort: sortOrder, ids: selectedCompanyIds.join(",") },
+        });
+
+        setLoadedData(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [myCompany, compareCompanies]);
+
   return (
     <section className={styles.form}>
       <div className={styles.header}>
         <h4 className={styles.title}>비교 결과 확인하기</h4>
         <SelectBox options={basicSortOptions} />
       </div>
+      <StartupList startups={loadedData} />
     </section>
   );
 }
